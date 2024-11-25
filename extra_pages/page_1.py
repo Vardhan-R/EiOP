1c06fcf0f8613a0574c712de161202de466b2637
Vardhan R <vrdhnr@gmail.com>
2024-11-25 20:48:46 +0530

    Add files via upload

---
+++ extra_pages/page_1.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+main_col_1, main_col_2 = st.columns([1, 1], gap="large")
+
+with main_col_1:
+	st.header("Fap Tracker (2024)")
+	st.write("Yes")
+
+with main_col_2:
+	st.header("H2 BITCH")
+	st.write("Heyyy")

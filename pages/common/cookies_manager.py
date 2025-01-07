e4285770d99a47853cfbd3bab200d7e289515ef4
Vardhan-R <82092439+Vardhan-R@users.noreply.github.com>
2025-01-08 01:49:51 +0530

    Persistent login on PC only

--- pages/common/cookies_manager.py
+++ pages/common/cookies_manager.py
@@ -1,12 +1,9 @@
 from streamlit_cookies_manager import EncryptedCookieManager
-from streamlit_cookies_manager import CookieManager
 import time
 
-def initCookies(wait_iters: int = 50, sleep_time: float = 0.1) -> CookieManager:
+def initCookies(wait_iters: int = 50, sleep_time: float = 0.1) -> EncryptedCookieManager:
 	# Create a global cookie manager with a consistent prefix and password
-	# cookies = EncryptedCookieManager(prefix="EiOP_test", password="some_insecure_password")
-	# cookies = EncryptedCookieManager(prefix="EiOP_test", password="some_insecure_password")
-	cookies = CookieManager(prefix="EiOP")
+	cookies = EncryptedCookieManager(prefix="EiOP_test", password="some_insecure_password")
 	for _ in range(wait_iters):
 		if cookies.ready():
 			break

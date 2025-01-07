a28944063f3159e53c8efeffa01b3fd8c062b50a
Vardhan-R <82092439+Vardhan-R@users.noreply.github.com>
2025-01-08 00:59:52 +0530

    Test for ready cookies modified

--- pages/common/cookies_manager.py
+++ pages/common/cookies_manager.py
@@ -1,6 +1,9 @@
 from streamlit_cookies_manager import EncryptedCookieManager
 
-def initCookies() -> EncryptedCookieManager:
+def initCookies(wait_iters: int = 10) -> EncryptedCookieManager:
 	# Create a global cookie manager with a consistent prefix and password
-	# cookies = EncryptedCookieManager(prefix="EiOP", password="some_secure_password")
-	return EncryptedCookieManager(prefix="EiOP", password="some_secure_password")
+	cookies = EncryptedCookieManager(prefix="EiOP", password="some_secure_password")
+	for _ in range(wait_iters):
+		if cookies.ready():
+			break
+	return cookies

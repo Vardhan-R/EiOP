806c09adb89d6f19b5050115348f439224f6c355
Vardhan-R <82092439+Vardhan-R@users.noreply.github.com>
2025-01-08 01:36:48 +0530

    Testing

--- pages/common/cookies_manager.py
+++ pages/common/cookies_manager.py
@@ -3,7 +3,7 @@ import time
 
 def initCookies(wait_iters: int = 50, sleep_time: float = 0.1) -> EncryptedCookieManager:
 	# Create a global cookie manager with a consistent prefix and password
-	cookies = EncryptedCookieManager(prefix="EiOP", password="some_secure_password")
+	cookies = EncryptedCookieManager(prefix="EiOP_test", password="some_insecure_password")
 	for _ in range(wait_iters):
 		if cookies.ready():
 			break

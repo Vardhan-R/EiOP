db3f2c945851d0076491309362cf6fbc08edce58
Vardhan-R <82092439+Vardhan-R@users.noreply.github.com>
2025-01-08 01:44:39 +0530

    Testing

--- pages/common/cookies_manager.py
+++ pages/common/cookies_manager.py
@@ -1,9 +1,12 @@
 from streamlit_cookies_manager import EncryptedCookieManager
+from streamlit_cookies_manager import CookieManager
 import time
 
-def initCookies(wait_iters: int = 50, sleep_time: float = 0.1) -> EncryptedCookieManager:
+def initCookies(wait_iters: int = 50, sleep_time: float = 0.1) -> CookieManager:
 	# Create a global cookie manager with a consistent prefix and password
-	cookies = EncryptedCookieManager(prefix="EiOP_test", password="some_insecure_password")
+	# cookies = EncryptedCookieManager(prefix="EiOP_test", password="some_insecure_password")
+	# cookies = EncryptedCookieManager(prefix="EiOP_test", password="some_insecure_password")
+	cookies = CookieManager(prefix="EiOP")
 	for _ in range(wait_iters):
 		if cookies.ready():
 			break
